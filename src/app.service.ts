3c31d5530bb25017ab8abbaaecde27a50b1e22cf
Truong Pham <truongpham2412.nd@gmail.com>
2023-12-21 19:59:11 +0700

    a

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Itx gear BE --> go to /api to read the docs'
   }
 }
